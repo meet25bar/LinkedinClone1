10fa3887640f30c2cd7e356b69b92b32ec8e9d21
Meet Barot <147968753+meet25bar@users.noreply.github.com>
2025-07-09 22:08:20 +0530

    Update Navitems.tsx

--- Component/Navitems.tsx
+++ Component/Navitems.tsx
@@ -1,59 +1,57 @@
-import { Bell, BriefcaseBusiness, Home, icons, MessageCircleMore, Users } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
-// type NAVitems={
-//     src:string,
-//     icon:JSX.Element,
-//     text:string
-// }
+import { Bell, BriefcaseBusiness, Home, MessageCircleMore, Users } from 'lucide-react';
+import Link from 'next/link';
+import React, { ReactNode } from 'react';
 
-interface NAVitems{
-    src:string,
-    icon:JSX.Element,
-    text:string
+interface NavItem {
+  src: string;
+  icon: ReactNode;
+  text: string;
 }
-const navitems:NAVitems[] = [
-    {
-        src:'/home',
-        icon: <Home />,
-        text:"Home"
-    },
-    {
-        src:'/networks',
-        icon: <Users />,
-        text:"My Network"
-    },
-    {
-        src:'/job',
-        icon: <BriefcaseBusiness />,
-        text:"Jobs"
-    },
-    {
-        src:'/message',
-        icon: <MessageCircleMore />,
-        text:"Messaging"
-    },
-    {
-        src:'/notification',
-        icon: <Bell />,
-        text:"Notification"
-    }
-]
 
-const Navitems = () => {
+const navItems: NavItem[] = [
+  {
+    src: '/home',
+    icon: <Home />,  
+    text: 'Home',
+  },
+  {
+    src: '/networks',
+    icon: <Users />,  
+    text: 'My Network',
+  },
+  {
+    src: '/job',
+    icon: <BriefcaseBusiness />,  
+    text: 'Jobs',
+  },
+  {
+    src: '/message',
+    icon: <MessageCircleMore />,  
+    text: 'Messaging',
+  },
+  {
+    src: '/notification',
+    icon: <Bell />,  
+    text: 'Notifications',
+  },
+];
+
+const NavItems: React.FC = () => {
   return (
-    <div className='flex gap-8'>
-        {
-            navitems.map((navitems, index)=>{
-            return(<div key={index} className='flex flex-col items-center cursor-pointer text-[#666666] hover:text-black'>
-                <span>{navitems.icon}</span>
-                <Link className='text-xs' href={navitems.src}>{navitems.text}</Link>
-            </div>)
-            })
-        }
-      
+    <div className="flex gap-8">
+      {navItems.map((item, index) => (
+        <div
+          key={index}
+          className="flex flex-col items-center cursor-pointer text-[#666666] hover:text-black"
+        >
+          <span>{item.icon}</span>
+          <Link className="text-xs" href={item.src}>
+            {item.text}
+          </Link>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Navitems
+export default NavItems;
